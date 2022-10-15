c57d60a09e3ab7f36bc1a9b7bb99c6cda5981b37
Gabriela Rudarska <48833527+LadyVamp227@users.noreply.github.com>
2022-10-15 12:50:22 +0300

    Add files via upload
    add the tree needed resources

---
+++ backend-dev.tf
@@ -0,0 +1,28 @@
+resource "digitalocean_droplet" "backend-dev" {
+  image  = "ubuntu-20-04-x64"
+  name   = "backend-dev"
+  region = "fra1"
+  size   = "s-1vcpu-1gb"
+  ssh_keys = [
+    data.digitalocean_ssh_key.terraform.id
+  ]
+  connection {
+    host = self.ipv4_address
+    user = "root"
+    type = "ssh"
+    private_key = file(var.pvt_key)
+    timeout = "2m"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "export PATH=$PATH:/usr/bin",
+      # install nginx
+      "sudo apt update",
+      "sudo apt install -y git",
+      "sudo apt install -y docker.io",
+      "curl -L \"https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)\" -o /usr/local/bin/docker-compose",
+      "chmod 777 /usr/local/bin/docker-compose"
+    ]
+  }
+}
