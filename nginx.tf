7783ae5ee8395988d36b7e999a54577c57b187ef
gabriela <gar@peytz.dk>
2022-10-17 10:33:48 +0300

    Add ansible for managing the system packages for the droplets for now just for the nginx after that i will move to the other instances

--- nginx.tf
+++ nginx.tf
@@ -1,25 +1,25 @@
 resource "digitalocean_droplet" "nginx" {
-  image  = "ubuntu-20-04-x64"
-  name   = "nginx"
-  region = "fra1"
-  size   = "s-1vcpu-1gb"
+  image    = "ubuntu-20-04-x64"
+  name     = "nginx"
+  region   = "fra1"
+  size     = "s-1vcpu-1gb"
   ssh_keys = [
     data.digitalocean_ssh_key.terraform.id
   ]
+
   connection {
-    host = self.ipv4_address
-    user = "root"
-    type = "ssh"
+    host        = self.ipv4_address
+    user        = "root"
+    type        = "ssh"
     private_key = file(var.pvt_key)
-    timeout = "2m"
+    timeout     = "2m"
   }
 
   provisioner "remote-exec" {
-    inline = [
-      "export PATH=$PATH:/usr/bin",
-      # install nginx
-      "sudo apt update",
-      "sudo apt install -y nginx"
-    ]
+    inline = ["sudo apt update"]
+  }
+
+  provisioner "local-exec" {
+    command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u root -i '${self.ipv4_address},' --private-key ${var.pvt_key}  ansible/nginx.yaml"
   }
 }
